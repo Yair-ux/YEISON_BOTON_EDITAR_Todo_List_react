ed3bf27e34c8e01f9d9fc9060f808220b2649681
alyconr <alyconr>
2025-09-06 07:51:05 -0500

    Agregado botón de editar en TodoItem y función editarTarea

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,14 @@ import { useState } from "react"
 export default function App() {
 
   const [tareas, setTareas] = useState([]);
-
   const [input, setInput] = useState("");
 
-
   const agregarTarea = () => {
-
     if (input.trim()) {
       setTareas([...tareas, { id: Date.now(), text: input.trim(), completed: false }]);
       setInput("");
     };
-
   }
-
 
   const toggleCompleted = (id) => {
     setTareas(
@@ -26,24 +21,50 @@ export default function App() {
     );
   };
 
-
   const eliminarTarea = (id) => {
     setTareas(tareas.filter((tarea) => tarea.id !== id));
-
   }
 
+  // 👇 Nueva función para editar texto
+  const editarTarea = (id, nuevoTexto) => {
+    setTareas(
+      tareas.map((tarea) =>
+        tarea.id === id ? { ...tarea, text: nuevoTexto } : tarea
+      )
+    );
+  };
+
   return (
-    <div className="max-w-md mx-auto mt-10 p-2  rounded shadow">
+    <div className="max-w-md mx-auto mt-10 p-2 rounded shadow">
       <h1 className="text-3xl font-bold mb-5 text-center">LISTA DE TAREAS</h1>
       <div className="flex gap-3 mb-5">
-        <input className="flex-1 p-2 border rounded" type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Añadir Tarea" />
-        <button className="bg-blue-500 text-white px-4 p-y-2 rounded" onClick={agregarTarea} >Añadir Tareas</button>
+        <input
+          className="flex-1 p-2 border rounded"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Añadir Tarea"
+        />
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={agregarTarea}
+        >
+          Añadir Tareas
+        </button>
       </div>
 
       <div className="space-y-2">
-        {tareas.map((tarea) => (<TodoItem key={tarea.id} tarea={tarea} toggleCompleted={toggleCompleted} eliminarTarea={eliminarTarea} />))}
+        {tareas.map((tarea) => (
+          <TodoItem
+            key={tarea.id}
+            tarea={tarea}
+            toggleCompleted={toggleCompleted}
+            eliminarTarea={eliminarTarea}
+            //Pasamos las funciones como props al hijo
+            editarTarea={editarTarea}   
+          />
+        ))}
       </div>
-
     </div>
   )
 }
